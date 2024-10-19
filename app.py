1da3c445da8e96caa40c836122a08d7d80debba9
NithinAruva <121952066+NithinAruva@users.noreply.github.com>
2024-10-19 21:22:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from langchain_community.vectorstores import FAISS
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_google_genai import ChatGoogleGenerativeAI
 
 def get_pdf_text(pdf_docs):
